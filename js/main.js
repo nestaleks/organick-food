4e3e3e949fd5ba48327ee05880d75f5b02eeac3e
nestaleks <nestaleks83@gmail.com>
2024-02-08 10:18:47 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// СЛАЙДЕР
 $(document).ready(function(){
     $('.review__slides').slick();
 });
@@ -9,6 +10,8 @@ $('.review__slides').slick({
     fade: true,
     cssEase: 'linear'
 });
+
+// БУРГЕР МЕНЮ
 const menuBtn = document.querySelector('.menu__btn');
 const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.menu__list');
@@ -22,3 +25,27 @@ menuClose.addEventListener('click', ()=>{
     menuList.classList.remove('menu__list--open');
     menuShadow.classList.remove('menu--open');
 });
+
+// ФИЛЬТР КАТЕГОРИЙ ТОВАРОВ
+const filterContainer = document.querySelector(".products__category-list"),
+ productItems = document.querySelectorAll(".products__item");
+
+ filterContainer.addEventListener("click", (event) =>{
+   if(event.target.classList.contains("products__category-item")){
+   	 // deactivate existing active 'products__category-item'
+   	 filterContainer.querySelector(".active").classList.remove("active");
+   	 // activate new 'products__category-item'
+   	 event.target.classList.add("active");
+   	 const filterValue = event.target.getAttribute("data-filter");
+   	 productItems.forEach((item) =>{
+       if(item.classList.contains(filterValue) || filterValue === 'all'){
+       	item.classList.remove("hide");
+       	 item.classList.add("show");
+       }
+       else{
+       	item.classList.remove("show");
+       	item.classList.add("hide");
+       }
+   	 });
+   }
+ });
